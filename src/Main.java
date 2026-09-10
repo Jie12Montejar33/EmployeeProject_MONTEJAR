292a0f8e193eafcd4ae2c2869a7defbf65416bf7
User <knightmintmonte@gmail.com>
2026-09-10 11:09:40 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,28 @@ public class Main {
         h1.displayHourlyEmployee();
         double r = h1.computeSalary();
         System.out.println(r);
+        h1.setEmpID(67);
+        h1.displayHourlyEmployee();
 
 
         PieceWorkerEmployee p1 = new PieceWorkerEmployee(12, "me", 34, 12);
         p1.displayPieceWorkerEmployee();
         double r2 = p1.computeSalary();
         System.out.println(r2);
+        p1.setRatePerPiece(1200);
         System.out.println(p1.toString());
 
 
 
         CommisionEmployee em1 = new CommisionEmployee(13, "andrei",1200);
         em1.displayComissionEmployee();
-
+        em1.setTotalSale(2000);
+        em1.displayComissionEmployee();
         System.out.println(em1.computeSalary());
 
         BasePlusCommisonEmployee b1 = new BasePlusCommisonEmployee(3, "medd", 134, 50);
+        b1.displayBasePlusCommissionEmployee();
+        b1.setEmpName("none");
         b1.displayBasePlusCommissionEmployee();
         double ls34 = b1.computeSalary();
 
